2ef82d0583ce4ef0fd4bf38317af6cf01e66de82
Administrator <327590809@qq.com>
2020-06-09 17:35:37 +0800

    提交到本地仓库

---
+++ Github/src/gitproject/git.java
@@ -0,0 +1,23 @@
+package gitproject;
+
+public class git {
+	
+	
+	
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	
+	private  git (int id, String username, String password, int sex, String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.love = love;
+	}
+
+}
